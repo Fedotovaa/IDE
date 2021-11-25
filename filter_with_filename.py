f14f1260c39693706f2e9e81cae1569570bd1f0b
Fedotovaa <so.f.iy@mail.ru>
2021-11-25 17:01:20 +0500

    all steps

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -3,6 +3,14 @@ import numpy as np
 
 
 def convert_image_to_mosaic(image, size, gradation_step):
+    """
+
+    :param image: image
+    :param size: int
+    :param gradation_step: int
+    :return: image
+
+    """
     for x in range(0, len(image), size):
         for y in range(0, len(image[0]), size):
             image[x:x + size, y:y + size] = get_average_brightness(
@@ -11,6 +19,16 @@ def convert_image_to_mosaic(image, size, gradation_step):
 
 
 def get_average_brightness(block, size, gradation_step):
+    """
+
+    :param block: int
+    :param size: int
+    :param gradation_step: int
+    :return: int
+
+    >>> get_average_brightness(np.array(Image.open("scale1200.jpg"))[0:0 + 10, 0:0 + 10],10,50)
+    150
+    """
     average_color = (block[:size, :size].sum() / 3) // size ** 2
     return int(average_color // gradation_step) * gradation_step
 
@@ -27,4 +45,7 @@ def main():
 
 
 if __name__ == '__main__':
+    import filter_with_filename
+
     main()
+    filter_with_filename.testmod()
